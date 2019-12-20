21b4807484d51931b3d1fdaf1cd7e41a5a905f2d
Afonso Dutra Nogueira Filho <afonsoft@gmail.com>
2019-12-20 12:21:09 -0300

    Update ParallelUnitTest.cs

--- test/ParallelUnitTest.cs
+++ test/ParallelUnitTest.cs
@@ -15,11 +15,11 @@ namespace Afonsoft.Parallel.UnitTest
             {
                 Console.WriteLine($"{e.LogLevel} - {e.Description}");
             };
+            operationQueue.TaskStarted += (s, e) => { Console.WriteLine($"TaskStarted - {e.StartTimestamp} - {e.Context.ToString()}"); };
+            operationQueue.TaskCompleted += (s, e) => { Console.WriteLine($"TaskCompleted - {e.CompletionTimestamp} - {e.Context.ToString()}"); };
+            operationQueue.WorkerError += (s, e) => { Console.WriteLine($"WorkerError - {e.TaskException} - {e.Context.ToString()}"); };
 
-            operationQueue.TaskCompleted += (s, e) => { Console.WriteLine($"TaskCompleted - {e.CompletionTimestamp}"); };
-            operationQueue.WorkerError += (s, e) => { Console.WriteLine($"WorkerError - {e.TaskException}"); };
-
-            for (int i = 0; i <= 20; ++i)
+            for (int i = 0; i <= 100; ++i)
                 operationQueue.Enqueue(new WorkerContextTest() { Index = i.ToString() });
 
             operationQueue.Start();
@@ -32,30 +32,29 @@ namespace Afonsoft.Parallel.UnitTest
 
     public class WorkerTest : Worker<WorkerContextTest>
     {
-        private Guid guid;
-
         public override void Initialize()
         {
-            guid = Guid.NewGuid();
-            Console.WriteLine($"{this.Context.Index} - {guid.ToString()} - Initialize");
+            Console.WriteLine($"{this.Context.Index} - Initialize");
         }
 
         public override void Task()
         {
-            Console.WriteLine($"{this.Context.Index} - {guid.ToString()} - Start");
             Thread.Sleep(new Random().Next(1000, 10000));
-            Console.WriteLine($"{this.Context.Index} - {guid.ToString()} - End");
-            Thread.Sleep(1000);
         }
 
         public override void Terminate()
         {
-            Console.WriteLine($"{this.Context.Index} - {guid.ToString()} - Terminate");
+            Console.WriteLine($"{this.Context.Index} - Terminate");
         }
     }
 
     public class WorkerContextTest
     {
         public string Index { get; set; }
+
+        public override string ToString()
+        {
+            return Index;
+        }
     }
 }
